39b742ed01d2a8042688a0027f2f576f2acdc532
Alp Senay <alps2789@nycstudents.net>
2023-05-21 23:07:38 -0400

    working...

--- src/MainGUIWindow.java
+++ src/MainGUIWindow.java
@@ -1,38 +1,89 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class MainGUIWindow extends JFrame implements ActionListener {
+public class MainGUIWindow extends JFrame implements ActionListener, KeyListener {
     private JPanel mainPanel;
-    private JTextField a0TextField2;
-    private JTextField a0TextField1;
-    private JTextField a0TextField;
+    private JTextField numPeopTextField;
+    private JButton addNumPeopButton;
+    private JButton subtractNumButton;
+    private JTextField tipTextField;
+    private JTextField billTextField;
+    private JTextArea textAreaTip;
+    private JTextArea textAreaTotal;
     private JButton subtractTipButton;
     private JButton addTipButton;
-    private JButton subtractPeopleButton;
-    private JButton addPeopleButton;
-    private JTextArea textArea1;
-    private JTextArea textArea2;
+    private JButton saveLikeSethButton;
+    private int tipPercent;
+    private int numPeople;
+    private double bill;
+
 
     public MainGUIWindow() {
         createUIComponents();
+        tipPercent = 1;
+        numPeople = 1;
+        bill = 1;
     }
 
     private void createUIComponents() {
         setContentPane(mainPanel);
         setTitle("Tip Calculator GUI");
-        setSize(900, 600);
+        setSize(500, 250);
         setLocation(500,50);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         subtractTipButton.addActionListener(this);
         addTipButton.addActionListener(this);
-        subtractPeopleButton.addActionListener(this);
-        addPeopleButton.addActionListener(this);
+        addNumPeopButton.addActionListener(this);
+        subtractNumButton.addActionListener(this);
+        tipTextField.addKeyListener(this);
+        billTextField.addKeyListener(this);
+        numPeopTextField.addKeyListener(this);
         setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        if (source instanceof JButton button) {
+            if (button.equals(addTipButton)) {
+                tipPercent++;
+            }
+            if (button.equals(subtractTipButton)) {
+                tipPercent--;
+            }
+            if (button.equals(addNumPeopButton)) {
+                numPeople++;
+            }
+            if (button.equals(subtractNumButton)) {
+                numPeople--;
+            }
+            if (button.equals(saveLikeSethButton)) {
+                bill = Double.parseDouble(billTextField.getText());
+            }
+
+            textAreaTip.setText((int) (bill*tipPercent/numPeople)/100.0+"");
+            textAreaTotal.setText("abc"+((bill*(1+tipPercent/100.0))/numPeople));
+
+            tipTextField.setText(Integer.toString(tipPercent));
+            numPeopTextField.setText(""+numPeople);
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
 
     }
 }
